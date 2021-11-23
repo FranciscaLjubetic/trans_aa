5e82d9c934c7827e55e3ab8921b02e929f010d19
Francisca <franciscaljubetic@gmail.com>
2021-11-23 09:30:27 -0300

    no esta listo, pero se commitea para poder probar el script de php

--- formulario.php
+++ formulario.php
@@ -1,19 +1,40 @@
 <?php
-error_reporting(0);
-$nombre = $_POST['nombre'];
-$correo_electronico= $_POST['email'];
-$header = 'From: ' . $mail . ";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado 
+<?php
+if(isset($_POST['email'])) {
 
-$para = AQUÍ PONES TU CORREO;
-$asunto = 'AQUÍ LO QUE QUIERAS';
+<bold>// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias</bold>
+$email_to = "franciscaljubetic@gmail.com";
+$email_subject = "Contacto desde el sitio web";
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+<bold>// Aquí se deberían validar los datos ingresados por el usuario</bold>
+if(!isset($_POST['nombre']) ||
+!isset($_POST['correo']) ||
+!isset($_POST['telefono']) ||
+!isset($_POST['trip_start']) ||
+!isset($_POST['appt']) ||
+!isset($_POST['mensaje'])) {
 
-echo 'mensaje enviado correctamente';
+messages.error("<b>Por favor, llena todos los campos</b><br />") ;
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
+$email_message .= "Mail: " . $_POST['correo'] . "\n";
+$email_message .= "telefono: " . $_POST['telefono'] . "\n";
+$email_message .= "fecha del viaje: " . $_POST['trip_start'] . "\n";
+$email_message .= "hora del viaje: " . $_POST['appt'] . "\n\n";
+$email_message .= "Mensaje: " . $_POST['mensaje'] . "\n\n";
+
+<bold>// Ahora se envía el e-mail usando la función mail() de PHP</bold>
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
 
 ?>
